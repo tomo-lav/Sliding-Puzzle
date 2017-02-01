8edd4289b1a57239216ebf200321085de50c8a2a
Tomoya Miyara <e165709@ie.u-ryukyu.ac.jp>
2017-02-01 17:46:13 +0900

    make a print-board method

--- src/main/java/e165709/Board.java
+++ src/main/java/e165709/Board.java
@@ -12,11 +12,21 @@ public class Board {
             for (int n = 0; n < 3; n++){
                 board[i][n] = a;
                 a++;
+                System.out.println(board[i][n]);
             }
         }
-        //System.out.println(board[i][n]);
-
-
     }
+    public void printboard(){
+        int[][] a = {{0,1,2},{3,4,5},{6,7,8}};
 
+        for (int row = 0; row < 3; row++) {
+            for (int column = 0; column < 3; column++) {
+                if (column == 2) {
+                    System.out.printf(" %d\n", a[row][column]);
+                } else {
+                    System.out.printf(" %d", a[row][column]);
+                }
+            }
+        }
+    }
 }
